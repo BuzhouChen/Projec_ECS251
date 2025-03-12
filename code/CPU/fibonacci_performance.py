8b069d32ff9cce62c6383b68020cdf2b2991dfdf
dormantflower <xypjiang@ucdavis.edu>
2025-03-11 19:59:06 -0700

    Update fibonacci_performance.py

--- code/CPU/fibonacci_performance.py
+++ code/CPU/fibonacci_performance.py
@@ -1,4 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+import psutil
 
 # Fibonacci function
 def fibonacci(n):
@@ -6,15 +8,25 @@ def fibonacci(n):
         return n
     return fibonacci(n-1) + fibonacci(n-2)
 
-# get the execution time and throughput
-def performance(executor, num_workers, task_num):
+# get the performance metrics
+# runtime, throughput, cpu, memory
+# executor_type: ThreadPoolExecutor or ProcessPoolExecutor
+def performance(executor_type, num_workers, task_num):
     start_time = time.time()
-    execute(executor, num_workers, task_num)
-    end_time = time.time()
-    time = end_time - start_time
-    throughput = task_num / time
+    process = psutil.Process()  
+    initial_cpu = process.cpu_percent(interval=None)  
+    initial_memory = process.memory_info().rss  
 
-    return time, throughput
+    execute(executor_type, num_workers, task_num)
+    end_time = time.time()
+    final_cpu = process.cpu_percent(interval=None)  
+    final_memory = process.memory_info().rss  
+    runtime = end_time - start_time
+    throughput = task_num / runtime
+    cpu = final_cpu - initial_cpu
+    mem = final_memory - initial_memory
+
+    return runtime, throughput, cpu, mem
 
 def execute(executor, num_workers, task_num):
     with executor(max_workers=num_workers) as executor:
@@ -22,18 +34,22 @@ def execute(executor, num_workers, task_num):
         results = [future.result() for future in futures]
 
 def main():
-    task_num = 20  # number of tasks
+    task_num = 200  # number of tasks
     num_workers = 4  # number of threads/processes
 
     print("ThreadPoolExecutor:")
-    thread_time, thread_throughput = performance(ThreadPoolExecutor, num_workers, task_num)
+    thread_time, thread_throughput, thread_cpu, thread_mem = performance(ThreadPoolExecutor, num_workers, task_num)
     print(f"Execution Time: {thread_time:.4f} s")
     print(f"Throughput: {thread_throughput:.4f} tasks/s\n")
+    print(f"CPU usage: {thread_cpu:.2f}%")
+    print(f"Memory usage: {thread_mem / (1024 ** 2):.2f} MB\n")
 
     print("ProcessPoolExecutor:")
-    process_time, process_throughput = performance(ProcessPoolExecutor, num_workers, task_num)
+    process_time, process_throughput, process_cpu, process_mem = performance(ProcessPoolExecutor, num_workers, task_num)
     print(f"Execution Time: {process_time:.4f} s")
     print(f"Throughput: {process_throughput:.4f} tasks/s\n")
+    print(f"CPU usage: {process_cpu:.2f}%")
+    print(f"Memory usage: {process_mem / (1024 ** 2):.2f} MB\n")
 
 
 if __name__ == "__main__":
